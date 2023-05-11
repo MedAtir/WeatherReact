e819054ff8a45e99057f43c61f82012b12932894
MedAtir <50084280+MedAtir@users.noreply.github.com>
2023-05-11 14:45:48 +0100

    updated everything

--- weather_react_app/src/components/Today.js
+++ weather_react_app/src/components/Today.js
@@ -1,81 +1,50 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import pressure from '../assets/pressure.svg'
-import wind_speed from '../assets/wind_speed.svg'
-import humidity from '../assets/humidity.svg'
-import sunrise from '../assets/sunrise.svg'
-import sunset from '../assets/sunset.svg'
-import CardContent from '@material-ui/core/CardContent';
+import React from "react";
 
+import pressure from "../assets/pressure.svg";
+import wind_speed from "../assets/wind_speed.svg";
+import humidity from "../assets/humidity.svg";
+import sunrise from "../assets/sunrise.svg";
+import sunset from "../assets/sunset.svg";
+import { Card, Col, Row, Container } from "react-bootstrap";
+import Font from 'react-font'
+function Today({ today }) {
+  return (
+    <>
+       <Font family='Ubuntu'>
+      <Row className="border bg-light p-2">
+        <Col className="fs-2 fw-bolder bg-info  text-light">{today.temp}°C</Col>
+        <Col className=" bg-primary text-light">
+          {today.main}, {today.desc}
+        </Col>
+        <Col className="fs-4 fw-bolder">
+          {" "}
+          <img src={sunrise} alt="Logo" className="img-fluid col-6" />{" "}
+          {today.sunrise} A.M{" "}
+        </Col>
+        <Col className="fs-4 fw-bolder">
+          <img src={sunset} alt="Logo" className="img-fluid col-6" />{" "}
+          {today.sunset} P.M
+        </Col>
 
-const useStyles = makeStyles((theme) => ({
-    unit__icon: {
-        width: 22,
-        height:22,
-        alignSelf: 'center',
-        marginRight: 4,
-        marginLeft: 20,
-    },
-    unit__icon1: {
-        width: 22,
-        height:22,
-        alignSelf: 'center',
-        fontSize: '15',
-    },
-    weather__icon: {
-        width: 90,
-        height: 90,
-        Top: 0,
-    },
-    main : {
-        overflow: 'auto',
-        padding: 5,
-    },
-    text__left: {
-        float: 'left',
-    },
-    text__right: {
-        float: 'right',
-    },
-    span: {
-        fontWeight: 'bold',
-    }
-
-  }));
-
-function Today({today}) {
-    const classes = useStyles();
-    return (
-        <CardContent>
-            <div className={classes.main}>
-                <div className={classes.text__left}>
-                    <img src={`https://openweathermap.org/img/w/${today.icon}.png`} alt={today.icon} className={classes.weather__icon}/>
-                    <Typography variant="h3" gutterBottom >
-                        {today.temp}°C
-                    </Typography>
-                    <Typography variant="h6" gutterBottom>
-                        {today.main}, {today.desc}
-                    </Typography>
-                </div>
-                <div className={classes.text__right}>
-                    
-                    <Typography variant="h6"gutterBottom >
-                       <img src={sunrise} alt="Logo" className={classes.unit__icon}/> {today.sunrise} A.M.
-                    </Typography>
-                    <Typography variant="h6"gutterBottom >
-                        <img src={sunset} alt="Logo" className={classes.unit__icon}/> {today.sunset} P.M.
-                    </Typography>
-                    
-                </div>
-            </div>
-                <div>
-                    <img src={pressure} alt="Logo" className={classes.unit__icon1}/><span className={classes.span}>{today.pressure} hPa</span>
-                    <img src={humidity} alt="Logo" className={classes.unit__icon}/><span className={classes.span}>{today.humidity} %</span>
-                    <img src={wind_speed} alt="Logo" className={classes.unit__icon}/><span className={classes.span}>{today.wind} m/s N</span>
-                </div>
-            </CardContent>    
-    )
+       
+      </Row>
+      <Row className="mt-1 border p-5 bg-light ">
+      <Col>
+          <img src={pressure} alt="Logo" className="img-fluid col-4 " />
+          <span className="fs-2 fw-bolder">{today.pressure} hPa</span>
+        </Col>
+        <Col>  
+          <img src={humidity} alt="Logo" className="img-fluid col-4 " xs={12} />
+          <span className="fs-2 fw-bolder">{today.humidity} %</span>
+        </Col>
+        <Col>
+          <img src={wind_speed} alt="Logo" className="img-fluid col-4" />
+          <span className="fs-2 fw-bolder">{today.wind} m/s N</span>
+        </Col>
+      </Row>
+      </Font>
+    </>
+  );
 }
 
-export default Today
+export default Today;
--- weather_react_app/src/components/Weather.js
+++ weather_react_app/src/components/Weather.js
@@ -1,52 +1,32 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
-import LocalInfo from './LocalInfo';
-import Today from './Today';
-import Weekly from './Weekly'
+import React from "react";
 
+import LocalInfo from "./LocalInfo";
+import Today from "./Today";
+import Weekly from "./Weekly";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    marginTop: 60,
-    padding: 15,
+import { Card, Col, Row, Container } from "react-bootstrap";
 
-  },
-  card: {
-    padding: theme.spacing(2),
-  },
-  section: {
-    height: "100%",
-    paddingTop: 5,
-    backgroundColor: 'rgba(5,4,2,0.1)',
-  },
-}));
-
-function Weather({today, weekly}) {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={6}>
-        <Card className={classes.section} >
-          <LocalInfo today={today}/>
-        </Card>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <Card className={classes.section}>
-          <Today today={today}/>
-        </Card>
-        </Grid>
-        <Grid item xs={12}>
-          <Card className={classes.section}>
-            <Weekly weekData={ weekly } />
-          </Card>
-        </Grid>
-      </Grid>
-    </div>
-  )
+function Weather({ today, weekly }) {
+  return (   
+      <Container className="mt-5 mb-5 bg-opacity-50">
+        <Row>
+          <Col xs={{ order: "last" }}>
+            <Card className="bg-opacity-50">
+              <LocalInfo today={today} />              
+            </Card>
+          </Col>
+          <Col xs>
+            {" "}
+            <Today today={today} />
+          </Col>         
+        </Row>
+        <Row className="bg-light bg-opacity-50">
+          <Col>
+          <Weekly weekData={weekly}   className="Overflox"/>
+          </Col>      
+        </Row>
+      </Container>     
+    
+  );
 }
-export default Weather
+export default Weather;
